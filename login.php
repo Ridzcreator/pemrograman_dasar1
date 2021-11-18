9ade37552c4dd57220695ac05aa9f44172b9d542
NRidz <35356539+Ridzcreator@users.noreply.github.com>
2021-11-18 10:45:22 +0700

    Add files via upload

--- login.php
+++ login.php
@@ -2,17 +2,49 @@
 <html>
 <head>
     <title>Halaman Login</title>
+<style>
+	form{
+		text-align:center;
+	    }
+	body{
+		background: rgba(0, 128, 0, 0.3);
+	    }
+	.button {
+ 	background-color: #4CAF50;
+  	border: none;
+  	color: white;
+  	padding: 15px 32px;
+  	text-align: center;
+  	text-decoration: none;
+  	display: inline-block;
+  	font-size: 16px;
+  	margin: 4px 2px;
+  	cursor: pointer;
+	}
+
+.button1 {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #4CAF50;
+}
+
+.button1:hover {
+  background-color: #4CAF50;
+  color: white;}
+</style>
+
 </head>
 
 <body>
+<img src="smektis.jpg" height="400" width="1262">
     <div class="container">
-        <h1>Login</h1>
+        <h1 style="color:#2F86A6;text-align:center;">Login</h1>
         <form>
-            <label>Username</label><br>
+            <label style="color:#FFFF">Username</label><br>
             <input type="text"><br>
-            <label>Password</label><br>
-            <input type="password"><br>
-            <button>Log in</button>
+            <label style="color:#FFFF">Password</label><br>
+            <input type="password"><br><br>
+            <button  class="button button1">Log in</button>
         </form>
     </div>
 </body>
